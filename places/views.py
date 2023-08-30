21954c5c31374de76d264f7463b3992a0c0c46d0
Artuom4ik <masterskayaigr123@gmail.com>
2023-08-30 11:54:54 +0300

    Upload JSON

--- places/views.py
+++ places/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Place
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -32,6 +32,24 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+def get_images(place):
+    images = place.images.all()
+    images_url = []
+    for image in images:
+        images_url.append(image.image.url)
+    return images_url
+
+
 def place_detail_show(request, place_id):
     place = get_object_or_404(Place, id=place_id)
-    return HttpResponse(place.title)
+    place_description = {
+        "title": place.title,
+        "imgs": get_images(place),
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude
+        }
+    }
+    return JsonResponse(place_description, json_dumps_params={'ensure_ascii': False, 'indent': 2})
