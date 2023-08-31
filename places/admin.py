e904b993c2212429a1090fd3cde3ba690ed376e5
Artuom4ik <masterskayaigr123@gmail.com>
2023-08-31 18:59:05 +0300

    Add preview

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableStackedInline
+from django.utils.html import format_html
 
 from .models import Place, Image
 
 # Register your models here.
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["headshot_image"]
+    fields = ('image', 'headshot_image', 'image_number',)
+
+    def headshot_image(self, obj):
+        return format_html(f'<img src="{obj.image.url}" style="max-height: 200px; max-width: 200px;" >')
 
 
 @admin.register(Place)
